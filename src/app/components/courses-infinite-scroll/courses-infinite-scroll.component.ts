3bee55b9e194d738d4c050b4f9684fcea4b5e5d8
urbanojvr <u.villanueva95@gmail.com>
2019-04-15 15:22:28 +0200

    CoursesInfiniteScrollComponent shows table of courses but do nothing more. No button GoUp and no scrolling implemneted yet

--- src/app/components/courses-infinite-scroll/courses-infinite-scroll.component.ts
+++ src/app/components/courses-infinite-scroll/courses-infinite-scroll.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../../service/api/api.service';
+import {LogService} from '../../service/log/log.service';
+import {Subscription} from 'rxjs';
+import {Course} from '../../domain/course';
+import {Page} from '../../domain/pagedata/page';
+import {isEmpty} from 'rxjs/operators';
 
 @Component({
   selector: 'app-courses-infinite-scroll',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesInfiniteScrollComponent implements OnInit {
 
-  constructor() { }
+  private LOG_TAG = 'COURSES_INFINITE_SCROLL: ';
+  private PAGE_SIZE = 15;
 
-  ngOnInit() {
+  private courses: Course[];
+  private actualPage: number;
+  private totalPages: number;
+
+  private sub: Subscription;
+  private logService: LogService;
+
+  constructor(private apiService: ApiService) {
+    this.logService = new LogService(this.LOG_TAG);
+    this.actualPage = 0;
+    this.courses = new Array<Course>();
   }
 
+  getPageOfCourses(pageToGet: number) {
+    this.sub = this.apiService.getCourses(this.PAGE_SIZE, pageToGet).subscribe(
+      response => {
+        this.addCoursesToArray(response);
+        this.logService.printLogWithObject('Response from apiService:', response);
+      }, error => this.logService.print(error, LogService.ERROR_MSG));
+  }
+
+  addCoursesToArray(resp: Page<Course>) {
+    if (this.courses.length > 0) {
+      const coursesToAppend: Course[] = resp.content;
+      this.courses.concat(coursesToAppend);
+      this.logService.print('Courses appended = ' + coursesToAppend.length, LogService.DEFAULT_MSG);
+    } else {
+      this.courses = resp.content;
+      this.logService.print('Courses array initialized', LogService.DEFAULT_MSG);
+    }
+  }
+
+  ngOnInit() {
+    this.getPageOfCourses(this.actualPage);
+  }
+
+  editCourse(course: Course) {
+    this.logService.printLogWithObject('Edit course => ', course);
+  }
+
+  deleteCourse(course: Course) {
+    this.logService.printLogWithObject('Delete course => ', course);
+  }
 }
